31841d8ab1fd355f8973c7500e178cf321a34bd3
Gopalakrishnan Iyer <gopalakrishnan.iyer@aexp.com>
2024-12-01 20:08:51 -0700

    Day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,39 @@
+import kotlin.math.abs
+
+fun calculateTotalDistance(leftList: List<Int>, rightList: List<Int>): Int {
+    val sortedLeft = leftList.sorted()
+    val sortedRight = rightList.sorted()
+
+    var totalDistance = 0
+    for (i in sortedLeft.indices) {
+        totalDistance += abs(sortedLeft[i] - sortedRight[i])
+    }
+
+    return totalDistance
+}
+
+fun calculateSimilarityScore(leftList: List<Int>, rightList: List<Int>): Int {
+    var similarityScore = 0
+    for (leftNumber in leftList) {
+        val count = rightList.count { it == leftNumber }
+        similarityScore += leftNumber * count
+    }
+    return similarityScore
+}
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    val leftList = mutableListOf<Int>()
+    val rightList = mutableListOf<Int>()
+
+    for (line in input) {
+        val (left, right) = line.trim().split("\\s+".toRegex())
+        leftList.add(left.toInt())
+        rightList.add(right.toInt())
+    }
+
+    val totalDistance = calculateTotalDistance(leftList, rightList)
+    println("Total distance between lists: $totalDistance")
+    val similarityScore = calculateSimilarityScore(leftList, rightList)
+    println("Similarity Score: $similarityScore")
 }
